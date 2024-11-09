1e0c3bcf8a97df925c9e030204c50202d0f64959
Sidd03192 <135279087+Sidd03192@users.noreply.github.com>
2024-11-09 16:15:07 -0600

    updated with merge sort

--- Sets/test.java
+++ Sets/test.java
@@ -1,30 +1,28 @@
 import java.util.ArrayList;
 import java.util.Iterator;
-
+import java.util.Arrays;
 
 public class test {
     
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
-        
+        int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 };
+
         UnsortedSet<Double> al2 = new UnsortedSet<>();
         al2.add(17.0);
-         al2.add(10.0);
-         al2.add(11.0);
-      
-        al2.add(14.0);
-        al2.add(15.0);
-        al2.add(16.0);
-        
-        al2.add(12.0);
-        al2.add(13.0);
-        al2.add(2.0);
-        al2.add(20.0);
+        al2.add(19.0);
           
         SortedSet<Double> al = new SortedSet<>(al2);
         System.out.println(al);
-        System.out.println(al.get(16.5, 0, al.size()));
+
+        System.out.println("*********************");
+        al.clear();
+        System.out.println(al);
+        al.add(10.0);
+        al.add(9.0);
+        al.add(9.0);
+        System.out.println(al);
 
         
 
